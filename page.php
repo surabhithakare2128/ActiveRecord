4d116c145b685476fe945a754e573d4522791f7b
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-11-15 20:59:15 -0500

    page class

---
+++ page.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'stringFunctions.php';
+abstract class page{
+	public $html;
+	public function __construct(){
+		$this->html .= "<html>";
+		//$this->html .= "<link rel="stylesheet" href="styles.css">";
+		$this->html .= "<body>";
+	}
+	public function __destruct(){
+		$this->html .= "</body></html>";
+		stringFunctions::printThis($this->html);
+	}
+	public function get(){
+		echo "default get message";
+	}
+	public function post(){
+		print_r($_POST);
+	}
+}	
+
+?>
