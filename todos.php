017040e4a65e33cffa9fe3bd72543fe130e6a30b
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-11-18 16:54:55 -0500

    todos.php

---
+++ todos.php
@@ -0,0 +1,13 @@
+<?php
+
+class todos extends model {
+	public $id;
+        public $owneremail;
+	public $ownerid;
+	public $createddate;
+	public $duedate;
+	public $message;
+	public $isdone;
+}
+
+?>
