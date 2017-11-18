c39800c378d8f4f44503f4c81250a0253f302772
surabhi vasant thakare <st638@afsconnect1.njit.edu>
2017-11-18 16:58:38 -0500

    accounts.php

---
+++ accounts.php
@@ -0,0 +1,14 @@
+<?php
+
+class accounts extends model {
+	public $id;
+        public $email;
+	public $fname;
+	public $lname;
+	public $phone;
+	public $birthday;
+	public $gender;
+	public $password;
+}
+
+?>
